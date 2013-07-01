1efbe3614397867d9c1cb9c976f5ea39b6783613
Nikola Henezi <nikola.henezi@gmail.com>
2013-07-01 12:02:38 +0200

    argument parser added

--- pywmapper.py
+++ pywmapper.py
@@ -3,41 +3,59 @@
 import urllib2
 from validator import fullPath
 from bs4 import BeautifulSoup
+from argparse import ArgumentParser
 from collections import deque
+from pprint import pprint
 
-url = "test url"
-maxDepth = 1
-
-queue = deque([(url, 0)])
-info = {}
-visited = []
 
 def qtl(queue):
-  '''Converts depth queue to list of urls'''
+  '''Converts depth queue to list of urls
+     [(url1, 0), (url2, 1)] -> [url1, url2]
+  '''
   return map(lambda x: x[0], queue)
 
-while queue:
-  url, depth = queue.popleft()
-  print depth, "Visiting: ", url
-  visited.append(url);
-  info[url] = {}
-  try:
-    response = urllib2.urlopen(url)
-  except urllib2.HTTPError as e:
-    print "({0}): {1}".format(e.errno, e.strerror)
-    continue
-  html = BeautifulSoup(response.read())
+def run(options):
+  maxDepth = options.depth
+  queue = deque([(options.target, 0)])
+  info = {}
+  visited = []
 
-  if depth >= maxDepth:
-    continue
-  info[url]['src'] = html
-  info[url]['img'] = html.find_all('img')
-  info[url]['a'] = html.find_all('a')
-  for link in info[url]['a']:
-    if not link.get('href'):
+  while queue:
+    url, depth = queue.popleft()
+    print depth, "Visiting: ", url
+    visited.append(url);
+    info[url] = {}
+    try:
+      response = urllib2.urlopen(url)
+    except urllib2.HTTPError as e:
+      print "({0}): {1}".format(e.errno, e.strerror)
       continue
-    href = fullPath(url, link.get('href'));
-    if href == None:
+
+    html = BeautifulSoup(response.read())
+
+    if depth >= maxDepth:
       continue
-    if href not in visited and href not in qtl(queue):
-      queue.append((href, depth));
+    info[url]['src'] = html
+    info[url]['a'] = html.find_all('a')
+    for link in info[url]['a']:
+      if not link.get('href'):
+        continue
+      href = fullPath(url, link.get('href'));
+      if href == None:
+        continue
+      if href not in visited and href not in qtl(queue):
+        queue.append((href, depth));
+
+def parse():
+  parser = ArgumentParser(description='Website mapper')
+  parser.add_argument('-t', help='start address', action='store', dest='target')
+  parser.add_argument('-d', help='depth', action='store', dest='depth', type=int)
+
+  options = parser.parse_args()
+  if options.target[-1] != '/':
+    options.target += '/'
+
+  return options
+
+if __name__ == '__main__':
+  run(parse())
